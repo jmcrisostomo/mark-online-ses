d8c619a94dc2828a3b7229130fbe05ac42a87438
Mattheus <jm.crisostomo@gmail.com>
2023-02-11 13:32:32 +0800

    ⚙ fix: added for demo

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -207,7 +207,7 @@ class StudentController extends Controller
             return DataTables::of($data)
                 ->addIndexColumn()
                 ->addColumn('action', function ($row) {
-                    $actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
+                    $actionBtn = '<a href="javascript:void(0)" class="default btn btn-primary btn-sm">View Documents</a> <a href="javascript:void(0)" class="edit btn btn-success btn-sm">Approve</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Decline</a>';
                     return $actionBtn;
                 })
                 ->rawColumns(['action'])
